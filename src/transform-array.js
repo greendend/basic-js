0d6eb24967cd5ee9c57c438f0c225330c06330cc
greendend <alexakasmern@tut.by>
2020-03-19 20:49:03 +0300

    feat: transform-array task done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,40 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  
+    if (!Array.isArray(arr)) throw new Error();
+  
+    let result = [];
+    
+    //main idea: skip flags, concentrate on elements and what's them surround
+    for (let i = 0; i < arr.length; i++) {
+      switch(arr[i]) {
+        case '--discard-next':
+        case '--discard-prev':
+        case '--double-next':
+        case '--double-prev':
+          continue;
+      }
+  
+      result.push(arr[i]);
+  
+      switch(arr[i+1]) {
+        case '--double-prev':
+          result.push(arr[i]);
+          break;
+        case '--discard-prev':
+          result.splice(result.length-1,1);
+          break;
+      }
+  
+      switch(arr[i-1]) {
+        case '--double-next':
+          result.push(arr[i]);
+          break;
+        case '--discard-next':
+          result.splice(result.length-1,1);
+          break;
+      }
+    }
+  
+    return result;
+  
+  };
